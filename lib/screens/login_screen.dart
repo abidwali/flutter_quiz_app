d11ca1f4cd3d2cd5fddbd8ecd8418658aad504d7
Abid Wali <abidwali14@gmail.com>
2022-10-03 16:20:16 +0530

    added padding in login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -49,61 +49,66 @@ class _LoginPageState extends State<LoginPage> {
         title: const Text("Login Page"),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            TextFormField(
-              controller: _controllerEmail,
-              decoration: const InputDecoration(labelText: "Email"),
-            ),
-            TextFormField(
-              controller: _controllerPassword,
-              decoration: const InputDecoration(labelText: "Password"),
-            ),
-            Text(error),
-            Center(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(5),
-                child: Stack(
-                  children: [
-                    Positioned.fill(
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: <Color>[
-                              Color(0XFF0D47A1),
-                              Color(0XFF1976D2),
-                              Color(0XFF42A5F5),
-                            ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            
+            children: [
+              const SizedBox(height: 50,),
+              TextFormField(
+                controller: _controllerEmail,
+                decoration: const InputDecoration(labelText: "Email"),
+              ),
+              TextFormField(
+                controller: _controllerPassword,
+                decoration: const InputDecoration(labelText: "Password"),
+              ),
+              Text(error),
+              Center(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: <Color>[
+                                Color(0XFF0D47A1),
+                                Color(0XFF1976D2),
+                                Color(0XFF42A5F5),
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    TextButton(
-                      style: TextButton.styleFrom(
-                        padding: const EdgeInsets.all(5),
-                        foregroundColor: Colors.white,
-                        textStyle: const TextStyle(fontSize: 20),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          padding: const EdgeInsets.all(5),
+                          foregroundColor: Colors.white,
+                          textStyle: const TextStyle(fontSize: 20),
+                        ),
+                        onPressed: () {
+                          login ? loginUser() : createUser();
+                        },
+                        child: Text(login ? "Login" : "Create User"),
                       ),
-                      onPressed: () {
-                        login ? loginUser() : createUser();
-                      },
-                      child: Text(login ? "Login" : "Create User"),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            OutlinedButton(
-                onPressed: () {
-                  setState(() {
-                    login = !login;
-                  });
-                },
-                child: const Text("Switch Login/Create"))
-          ],
+              const SizedBox(
+                height: 10,
+              ),
+              OutlinedButton(
+                  onPressed: () {
+                    setState(() {
+                      login = !login;
+                    });
+                  },
+                  child: const Text("Switch Login/Create"))
+            ],
+          ),
         ),
       ),
     );
